daed3fc0400a3914249d7ca01b6639184ae9ca0a
KamilAdd-Byte <kamillodzinski09@gmail.com>
2020-10-13 23:08:00 +0200

    4 method in Manager: findAll, findById, saveBook, deleteBook

--- applibrary/src/main/java/com/responsywnie/applibrary/menager/BookManager.java
+++ applibrary/src/main/java/com/responsywnie/applibrary/menager/BookManager.java
@@ -17,16 +17,20 @@ public class BookManager {//Warstwa pośrednia między dostępem do danych a API
         this.bookRepo = bookRepo;
     }
 
-    public Optional<Book> findById(Long id) {//typ zwracany Optional. Metoda szuka po id.
+    public Optional<Book> findBookById(Long id) {//typ zwracany Optional. Metoda szuka po id.
         return bookRepo.findById(id);
     }
 
-    public Iterable<Book> findAll(Long id) {//typ zwracany Optional
+    public Iterable<Book> findAllBook(Long id) {//typ zwracany Optional
         return bookRepo.findAll();
     }
 
-    public Book saveBook(Book book) {//pozwala zapisywać
+    public Book saveBookInDatabase(Book book) {//pozwala zapisywać
         return bookRepo.save(book);
 
+    }
+
+    public void deleteBookInDatabase(Long id){//usuwa z bazy typ void
+        bookRepo.deleteById(id);
     }
 }
