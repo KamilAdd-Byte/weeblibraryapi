34312bff0cabeecaf7fcd6600ed7825216230696
KamilAdd-Byte <kamillodzinski09@gmail.com>
2020-10-29 18:33:56 +0100

    Add SWAGGER UI and SWAGGER2 in pom.xml

--- applibrary/src/main/java/com/responsywnie/applibrary/api/BookApi.java
+++ applibrary/src/main/java/com/responsywnie/applibrary/api/BookApi.java
@@ -1,9 +1,9 @@
-//package com.responsywnie.applibrary.api;
-//
-//import org.springframework.web.bind.annotation.*;
-//
-//@RestController
-//@RequestMapping("/api/library")
-//public class BookApi {
-//
-//}
+package com.responsywnie.applibrary.api;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/library")
+public class BookApi {
+
+}
--- applibrary/src/main/java/com/responsywnie/applibrary/controller/LibraryController.java
+++ applibrary/src/main/java/com/responsywnie/applibrary/controller/LibraryController.java
@@ -1,8 +1,16 @@
 package com.responsywnie.applibrary.controller;
 
+import com.responsywnie.applibrary.entity.Book;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class LibraryController {
 
+    @GetMapping("/books")
+    public List<Book> getAllBooks(){
+        return getAllBooks();
+    }
 }
